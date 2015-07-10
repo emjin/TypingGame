e48ac1a6445deb1fabadfa5d989408717238b824
emjin <emmaljin@yahoo.com>
2015-07-10 13:05:00 -0400

    When the start button is clicked, it goes to an activity which has a background picture of an Analemma

--- app/src/main/java/analemma/typinggame/MainActivity.java
+++ app/src/main/java/analemma/typinggame/MainActivity.java
@@ -1,9 +1,14 @@
 package analemma.typinggame;
 
+import android.app.SearchManager;
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -17,8 +22,24 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void begin(View view){
+        //An intent represents an app's "intent to do something", usually start another activity
+        Intent intent = new Intent(this, FallingLetters.class);
+        //Finish the intent
+        startActivity(intent);
+    }
+
+    private void openSearch() {
+        startActivity(new Intent(SearchManager.INTENT_ACTION_GLOBAL_SEARCH));
+    }
+
+    private void openSettings() {
+        startActivity(new Intent(Settings.ACTION_SETTINGS));
     }
 
     @Override
@@ -30,9 +51,12 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            openSettings();
+            return true;
+        } else if (id == R.id.action_search) {
+            openSearch();
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
+            return super.onOptionsItemSelected(item);
     }
 }
