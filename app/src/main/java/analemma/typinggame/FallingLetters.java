dc617c886b478df179a6b13b7b8152d9ff8b093b
anbarth <anna2nsb@gmail.com>
2015-07-19 19:52:08 -0400

    now, upon successfully typing a letter, a new letter will respawn at the top of the screen.

--- app/src/main/java/analemma/typinggame/FallingLetters.java
+++ app/src/main/java/analemma/typinggame/FallingLetters.java
@@ -21,8 +21,12 @@ public class FallingLetters extends ActionBarActivity implements KeyEvent.Callba
     private static final int LET_SIZE = 40;
     private static final int LET_SPACING = 1; //as a fraction of letter size
 
+    private RelativeLayout rl;
+    private int scrHeight; //bc i need it all over the place for whatever reason
+
     private int[] currLets; //letters that are currently on screen, as ints from 'A' to 'Z'
     private TextView[] lets;
+    private int[] positions; //positions for each letter. is this the smart way? nah. is it easy tho? yeaaaah.
     private int[] keyEvents = {KeyEvent.KEYCODE_A, KeyEvent.KEYCODE_B, KeyEvent.KEYCODE_C, KeyEvent.KEYCODE_D,
             KeyEvent.KEYCODE_E, KeyEvent.KEYCODE_F, KeyEvent.KEYCODE_G, KeyEvent.KEYCODE_H, KeyEvent.KEYCODE_I,
             KeyEvent.KEYCODE_J, KeyEvent.KEYCODE_K, KeyEvent.KEYCODE_L, KeyEvent.KEYCODE_M, KeyEvent.KEYCODE_N,
@@ -36,19 +40,17 @@ public class FallingLetters extends ActionBarActivity implements KeyEvent.Callba
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_falling_letters);//idk i think this line is not needed but dont feel ike removing
 
-
-
-
-
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         int scrWidth = size.x;
+        scrHeight = size.y;
         int numLets = (int)(scrWidth/((1+LET_SPACING)*LET_SIZE));
         lets = new TextView[numLets];
         currLets = new int[numLets];
+        positions = new int[numLets];
 
-        RelativeLayout rl = new RelativeLayout(this);
+        rl = new RelativeLayout(this);
         rl.setBackgroundColor(getResources().getColor(R.color.background));//black background
         //add our beautiful analemma
         ImageView bg = new ImageView(this);
@@ -56,41 +58,44 @@ public class FallingLetters extends ActionBarActivity implements KeyEvent.Callba
         bg.setAdjustViewBounds(true); //this makes the object's size match the actual image's size
         rl.addView(bg);
 
-        //code to force keyboard - off stackoverflow, who knows
-        /*rl.setFocusable(true);
-        rl.setFocusableInTouchMode(true);
-        InputMethodManager mgr = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-        mgr.showSoftInput(rl, InputMethodManager.SHOW_IMPLICIT);*/
-
+        //force keyboard to show. thanks stackoverflow
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.toggleSoftInput (InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
 
-        //makes letters appear
+        //inits the appropriate positions for each letter
         int pos = 0;
         for(int i=0;i<lets.length;i++){
-            lets[i] = new TextView(this);
-            TextView let = lets[i];
-            let.setTextSize(LET_SIZE);
-            //choose a letter
-            int letterNum = (int) (26*Math.random()) + 'A';
-            let.setText((char) letterNum + "");
-            currLets[i] = letterNum;
-
-            rl.addView(let);
-            let.setX(pos);
-            let.setY(0); //idk if this is necessary
-            let.setTextColor(getResources().getColor(R.color.letter));
+            positions[i] = pos;
             pos += (1+LET_SPACING)*LET_SIZE;
         }
 
-        //makes letters fall
+        //go letters go
         for(int i=0;i<lets.length;i++){
-            lets[i].animate().setStartDelay((long)(1000*Math.random()) + 2000).setDuration((long)(5000*Math.random())).y(size.y);
+            createLetter(i);
         }
 
         setContentView(rl);
 
 
+    }
+
+    //chooses and animates a letter for a given textview in lets
+    private void createLetter(int i){
+        //init
+        lets[i] = new TextView(this);
+        TextView let = lets[i];
+        let.setTextSize(LET_SIZE);
+        //choose a letter
+        int letterNum = (int) (26*Math.random()) + 'A';
+        let.setText((char) letterNum + "");
+        currLets[i] = letterNum;
+        //add to layout in proper location
+        rl.addView(let);
+        let.setX(positions[i]);
+        let.setY(0); //idk if this is necessary
+        let.setTextColor(getResources().getColor(R.color.letter));
+        //animate
+        lets[i].animate().setStartDelay((long)(1000*Math.random()) + 2000).setDuration((long)(8000*Math.random())).y(scrHeight);
     }
 
     //gets the keyevent associated with given capital letter
@@ -105,7 +110,7 @@ public class FallingLetters extends ActionBarActivity implements KeyEvent.Callba
 
             if(keyCode == getKeyEvent(currLets[i])){
                 lets[i].setText("");//for now, the text should just disappear
-                //later, ill make it reappear at the top
+                createLetter(i);
                 return true;
             }
 
