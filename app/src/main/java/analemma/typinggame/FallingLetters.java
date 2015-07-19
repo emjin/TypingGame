ba9f7529da64707119783428e3b4d67f870c06e6
anbarth <anna2nsb@gmail.com>
2015-07-19 18:51:12 -0400

    made code that should in theory listen to a keyboard for input and respond accordingly, but still no keyboard shows, so who knows.

--- app/src/main/java/analemma/typinggame/FallingLetters.java
+++ app/src/main/java/analemma/typinggame/FallingLetters.java
@@ -1,31 +1,46 @@
 package analemma.typinggame;
 
 import android.graphics.Point;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class FallingLetters extends ActionBarActivity {
+public class FallingLetters extends ActionBarActivity implements KeyEvent.Callback {
 
     private static final int LET_SIZE = 40;
     private static final int LET_SPACING = 1; //as a fraction of letter size
-    //private static final int
+
+    private int[] currLets; //letters that are currently on screen, as ints from 'A' to 'Z'
+    private TextView[] lets;
+    private int[] keyEvents = {KeyEvent.KEYCODE_A, KeyEvent.KEYCODE_B, KeyEvent.KEYCODE_C, KeyEvent.KEYCODE_D,
+            KeyEvent.KEYCODE_E, KeyEvent.KEYCODE_F, KeyEvent.KEYCODE_G, KeyEvent.KEYCODE_H, KeyEvent.KEYCODE_I,
+            KeyEvent.KEYCODE_J, KeyEvent.KEYCODE_K, KeyEvent.KEYCODE_L, KeyEvent.KEYCODE_M, KeyEvent.KEYCODE_N,
+            KeyEvent.KEYCODE_O, KeyEvent.KEYCODE_P, KeyEvent.KEYCODE_Q, KeyEvent.KEYCODE_R, KeyEvent.KEYCODE_S,
+            KeyEvent.KEYCODE_T, KeyEvent.KEYCODE_U, KeyEvent.KEYCODE_V, KeyEvent.KEYCODE_W, KeyEvent.KEYCODE_X,
+            KeyEvent.KEYCODE_Y, KeyEvent.KEYCODE_Z}; //ints representing key events
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_falling_letters);
+        setContentView(R.layout.activity_falling_letters);//idk i think this line is not needed but dont feel ike removing
 
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         int scrWidth = size.x;
         int numLets = (int)(scrWidth/((1+LET_SPACING)*LET_SIZE));
-        TextView[] lets = new TextView[numLets];
+        lets = new TextView[numLets];
+        currLets = new int[numLets];
 
         RelativeLayout rl = new RelativeLayout(this);
         rl.setBackgroundColor(getResources().getColor(R.color.background));//black background
@@ -35,14 +50,18 @@ public class FallingLetters extends ActionBarActivity {
         bg.setAdjustViewBounds(true); //this makes the object's size match the actual image's size
         rl.addView(bg);
 
+
         //makes letters appear
         int pos = 0;
         for(int i=0;i<lets.length;i++){
             lets[i] = new TextView(this);
             TextView let = lets[i];
             let.setTextSize(LET_SIZE);
+            //choose a letter
             int letterNum = (int) (26*Math.random()) + 'A';
             let.setText((char) letterNum + "");
+            currLets[i] = letterNum;
+
             rl.addView(let);
             let.setX(pos);
             let.setY(0); //idk if this is necessary
@@ -60,8 +79,24 @@ public class FallingLetters extends ActionBarActivity {
 
     }
 
-    private void setLetter(TextView let, RelativeLayout rl){
+    //gets the keyevent associated with given capital letter
+    private int getKeyEvent(int letter){
+        return keyEvents[letter - 'A'];
+    }
 
+    //implementation of KeyEvent.Callback method
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event){
+        for(int i=0;i<currLets.length;i++){
+
+            if(keyCode == getKeyEvent(currLets[i])){
+                lets[i].setText("");//for now, the text should just disappear
+                //later, ill make it reappear at the top
+                return true;
+            }
+
+        }
+        return false; //idk what returning false will do but it seems appropriate
     }
 
     @Override
