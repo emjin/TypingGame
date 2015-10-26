b9092ce959825f7b5b907aeeed312bc01f94ae6b
Emma Jin <emmaljin@yahoo.com>
2015-10-25 21:28:45 -0400

    Got rid of button, have them press back button

--- app/src/main/java/analemma/typinggame/InstructionsPage.java
+++ app/src/main/java/analemma/typinggame/InstructionsPage.java
@@ -3,6 +3,8 @@ package analemma.typinggame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class InstructionsPage extends AppCompatActivity {
@@ -13,8 +15,26 @@ public class InstructionsPage extends AppCompatActivity {
         setContentView(R.layout.activity_instructions_page);
     }
 
-    protected void back(View view) {
-        finish();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_instructions_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
